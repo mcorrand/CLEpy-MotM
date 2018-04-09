46bf902c7815f4a1e3da3cafdc221f05d4564ff2
mcorrand <matt.corrand@gmail.com>
2018-04-09 13:15:20 -0400

    v3

--- django_fsm/talk/app/tests.py
+++ django_fsm/talk/app/tests.py
@@ -1,6 +1,39 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.core.exceptions import ValidationError
 from django.test import TestCase
+from .models import Order
 
-# Create your tests here.
+
+class OrderTest(TestCase):
+    def test_lawful_return(self):
+        order = Order.objects.create(customer="Returny Customer",
+                                     address="1 main street",
+                                     item="blue lightsaber",
+                                     price=100,
+                                     is_shipped=True,
+                                     is_returned=True)
+        order.cancel()
+        self.assertTrue(order.is_cancelled)
+
+    def test_lawful_cancellation(self):
+        order = Order.objects.create(customer="Cancelly Customer",
+                                     address="1 main street",
+                                     item="blue lightsaber",
+                                     price=100,
+                                     is_shipped=False,
+                                     is_returned=False)
+        order.cancel()
+        self.assertTrue(order.is_cancelled)
+
+    def test_fraudster(self):
+        order = Order.objects.create(customer="Fraudy McFraudster",
+                                     address="1 main street",
+                                     item="blue lightsaber",
+                                     price=100,
+                                     is_shipped=True,
+                                     is_returned=False)
+
+        with self.assertRaises(ValidationError):
+            order.cancel()
